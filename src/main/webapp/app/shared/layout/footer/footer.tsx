13b8ebb5c3fd04c39b6742bbbc9221d527ae920c
yuchilai <yuchilai88@gmail.com>
2023-12-09 00:02:23 -0500

    Update footer to include 'All rights reserved' message.

--- src/main/webapp/app/shared/layout/footer/footer.tsx
+++ src/main/webapp/app/shared/layout/footer/footer.tsx
@@ -4,16 +4,20 @@ import React from 'react';
 import { Translate } from 'react-jhipster';
 import { Col, Row } from 'reactstrap';
 
-const Footer = () => (
-  <div className="footer page-content">
-    <Row>
-      <Col md="12">
-        <p>
-          <Translate contentKey="footer">Your footer</Translate>
-        </p>
-      </Col>
-    </Row>
-  </div>
-);
+const Footer = () => {
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <div className="footer page-content">
+      <Row>
+        <Col md="12">
+          <p>
+            © {currentYear} YuChi Lai. <Translate contentKey="footer.rights">All rights reserved.</Translate>
+          </p>
+        </Col>
+      </Row>
+    </div>
+  );
+};
 
 export default Footer;
